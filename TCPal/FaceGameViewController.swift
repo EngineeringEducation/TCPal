a1a554a43f379a400192edf36281d82b9361501d
Janardan Yri <janardan.yri@me.com>
2015-08-04 13:23:14 -0700

    First rev of face game thing with dummy data and simple UI.

--- TCPal/FaceGameViewController.swift
+++ TCPal/FaceGameViewController.swift
@@ -6,9 +6,59 @@
 //  Copyright © 2015 Janardan Yri. All rights reserved.
 //
 
+import QuartzCore
 import UIKit
 
 class FaceGameViewController: UIViewController {
+
+  // MARK: - Model
+
+  var persons = [
+    Person(name: "Bill Murray", faceURL: NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/0/0a/Bill_Murray%2C_Monuments_Men_premiere.jpg")!),
+    Person(name: "Stephen Hawking", faceURL: NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/e/eb/Stephen_Hawking.StarChild.jpg")!)
+  ]
+
+  // MARK: - "View Model"-ish
+
+  var currentPerson : Person! {
+    didSet {
+      self.faceView.image = self.currentPerson.face!
+    }
+  }
+
+  var nameChoices : [String]! {
+    didSet {
+      for i in (0..<self.buttons.count) {
+        self.buttons[i].setTitle(self.nameChoices[i], forState: .Normal)
+      }
+    }
+  }
+
+  // MARK: - Views
+
+  lazy var faceView : UIImageView = {
+    let imageView = UIImageView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+    imageView.clipsToBounds = true
+    imageView.contentMode = .ScaleAspectFill
+    imageView.layer.cornerRadius = 100
+    return imageView
+  }()
+
+  lazy var buttons : [UIButton] = {
+    return (0...1).map { index in
+      let button = UIButton(frame: CGRect(x: 50, y: 350 + 50 * index, width: 200, height: 50))
+      button.setTitleColor(UIColor.blackColor(), forState:.Normal)
+      return button
+    }
+  }()
+
+  lazy var confirmationLabel : UILabel = {
+    let label = UILabel(frame: CGRect(x: 100, y: 30, width: 300, height: 50))
+    label.textColor = UIColor.blackColor()
+    return label
+  }()
+
+  // MARK: - View Lifecycle
 
   override func loadView() {
     self.view = {
@@ -17,10 +67,62 @@ class FaceGameViewController: UIViewController {
       return view
     }()
 
-    self.view.addSubview({
-      let label = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 100))
-      label.text = "hi this is the game"
-      return label
-    }())
+    self.view.addSubview(self.faceView)
+
+    for button in self.buttons {
+      button.addTarget(self, action: "didTapNameButton:", forControlEvents: .TouchUpInside)
+      self.view.addSubview(button)
+    }
+
+    self.view.addSubview(self.confirmationLabel)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.loadFaces()
+  }
+
+  // MARK: - Happenin' Stuff
+
+  func loadFaces() {
+
+    var extantCount = self.persons.count
+    var failed = false
+
+    for person in self.persons {
+
+      person.getFace({ (success) -> Void in
+        if (!success) {
+          failed = true
+          print("failed :(")
+        }
+
+        if (--extantCount == 0) {
+          if (failed == false) {
+            self.throwUpNewPerson()
+          } else {
+            // FIXME: Do something when we've completed with partial success
+          }
+        }
+      })
+    }
+  }
+
+  func throwUpNewPerson() {
+    let randomIndex = Int(arc4random_uniform(UInt32(self.persons.count)))
+    self.currentPerson = self.persons[randomIndex]
+
+    // FIXME: randomize these
+    self.nameChoices = self.persons.map { person in
+      return person.name
+    }
+  }
+
+  func didTapNameButton(sender : UIButton) {
+    let buttonIndex = self.buttons.indexOf(sender)!
+    let selectedName = self.nameChoices[buttonIndex]
+    self.confirmationLabel.text = (selectedName == self.currentPerson.name) ? "Y" : "N"
+    self.throwUpNewPerson()
   }
 }
---
+++ TCPal/Person.swift
@@ -0,0 +1,38 @@
+//
+//  Person.swift
+//  TCPal
+//
+//  Created by Wren on 8/4/15.
+//  Copyright © 2015 Janardan Yri. All rights reserved.
+//
+
+class Person {
+  var face : UIImage?
+  let faceURL : NSURL
+  let name : String
+
+  init(name: String, faceURL: NSURL) {
+    self.faceURL = faceURL
+    self.name = name
+  }
+
+  func getFace(completion: (success: Bool) -> Void) {
+    let session = NSURLSession.sharedSession()
+    let task = session.dataTaskWithURL(self.faceURL) { (imageData, response, error) -> Void in
+      NSOperationQueue.mainQueue().addOperationWithBlock {
+        if let error = error {
+          print("Network error: \(error)")
+          completion(success:false)
+        } else if let imageData = imageData, image = UIImage(data: imageData) {
+          self.face = image
+          completion(success:true)
+        } else {
+          completion(success:false)
+        }
+      }
+    }
+
+    task.resume()
+  }
+
+}
